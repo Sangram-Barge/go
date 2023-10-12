ac136dceb5d66b7950996209fc8af80b0f9028d7
sbarge <sbarge@medline.com>
2023-10-12 11:04:28 +0530

    input validation

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,15 +7,15 @@ import (
 
 func main() {
   fmt.Println("--------------------------------------------------------------------------")
-	var conferenceName string = "Go Conference"
+  var conferenceName string = "Go Conference"
   const conferenceTickets uint8 = 50
   var remainingTickets uint8 = conferenceTickets
 
   bookings := []string{}
 
-	fmt.Printf("Welcome to %v booking app\n", conferenceName)
+  fmt.Printf("Welcome to %v booking app\n", conferenceName)
   fmt.Printf("We have total of %v tickets and %v tickets are available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+  fmt.Println("Get your tickets here to attend")
 
   var firstName string
   var lastName string
@@ -27,30 +27,42 @@ func main() {
     fmt.Scan(&firstName)
     fmt.Printf("Hello %v enter your last name\n", firstName)
     fmt.Scan(&lastName)
+     if !(len(firstName) >= 2 && len(lastName) >= 2) {
+      fmt.Printf("user name invalid, firstname and last name should be more than two characters\n")
+      fmt.Println("--------------------------------------------------------------------------")
+      continue
+    }
+
     fmt.Printf("Hello %v enter your email\n", firstName + " " + lastName)
     fmt.Scan(&email)
+    if !(strings.Contains(email, "@") && strings.Contains(email, ".com")) {
+      fmt.Printf("user email invalid must contain '@' and '.com'\n")
+      fmt.Println("--------------------------------------------------------------------------")
+      continue
+    } 
+
     fmt.Printf("Hello %v %v enter how many tickets you want\n", firstName, lastName)
     fmt.Scan(&userTickets)
 
-    if userTickets > remainingTickets {
+    if !(userTickets > 0 && userTickets <= userTickets) {
       fmt.Printf("Cannot book more than remaining Tickets, Remaining Tickets => %v\n", remainingTickets)
       fmt.Println("--------------------------------------------------------------------------")
       continue
     }
-    
+
     remainingTickets -= userTickets
     bookings = append(bookings, firstName + " " + lastName)
 
     fmt.Printf("Thank you %v for booking %v tickets. Ticket will be delivered at %v address\n", firstName + " " + lastName, userTickets, email)
     fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-    
+
     firstNames := []string{}
     for _, booking := range bookings {
       firstNames = append(firstNames, strings.Fields(booking)[0])
     }
     fmt.Printf("Theses are all the bookings in the app %v \n", firstNames)
     fmt.Println("--------------------------------------------------------------------------")
-    
+
     if remainingTickets == 0 {
       fmt.Println("We are full all tickets are booked, please come again next year")
       break
