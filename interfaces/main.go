92777c70c3fa1252b337477c5ef78fa271485d02
Sangram Barge <sangrambarge098@gmail.com>
2023-10-15 13:27:40 +0530

    interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+  "fmt"
+  "interfaces/shapes"
+)
+
+func main() {
+  s := shapes.Circle{
+    Radius: 3.4,
+  }
+
+  s2 := shapes.Circle{
+    Radius: 4.2,
+  }
+
+  rect1 := shapes.Rectangle{
+    Length: 9.8, 
+    Width: 8.6,
+  }
+
+  totalArea := calculateTotalArea(&s, &s2, &rect1)
+
+  fmt.Println("total area is", totalArea)
+}
+
+func calculateTotalArea(shapess ...shapes.Shape) (total float64){
+  for _, s := range shapess {
+    total += s.Area()
+  }
+  return total
+}
---
+++ interfaces/shapes/rect.go
@@ -0,0 +1,9 @@
+package shapes
+
+type Rectangle struct {
+  Length, Width float64
+}
+
+func (r *Rectangle) Area() float64 {
+  return r.Length * r.Width
+}
