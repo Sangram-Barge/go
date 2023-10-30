a8b922df56ffd784aa434d86196b3409a40be047
Sangram Barge <sangrambarge098@gmail.com>
2023-10-30 09:04:01 +0530

    uni directional channel

--- channels/main.go
+++ channels/main.go
@@ -14,19 +14,19 @@ func main() {
   fmt.Scanln(&t)
 }
 
-func pinger (c chan string) {
+func pinger (c chan<- string) {
   for {
     c <- "ping"
   }
 }
 
-func ponger (c chan string) {
+func ponger (c chan<- string) {
   for {
     c <- "pong"
   }
 }
 
-func printer (c chan string) {
+func printer (c <-chan string) {
   for {
     fmt.Println(<-c)
     time.Sleep(time.Second * 2)
