e897336096cb3af5b330a822b0e5ded2c8279d21
Sangram Barge <sangrambarge098@gmail.com>
2023-10-30 09:02:02 +0530

    bidirectional channel

---
+++ channels/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+  var c chan string = make(chan string)
+  go pinger(c) 
+  go printer(c) 
+  go ponger(c)
+  var t string
+  fmt.Scanln(&t)
+}
+
+func pinger (c chan string) {
+  for {
+    c <- "ping"
+  }
+}
+
+func ponger (c chan string) {
+  for {
+    c <- "pong"
+  }
+}
+
+func printer (c chan string) {
+  for {
+    fmt.Println(<-c)
+    time.Sleep(time.Second * 2)
+  }
+}
+
